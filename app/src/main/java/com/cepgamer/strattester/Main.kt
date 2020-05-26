48a7799cd4ba3c751a0c5b7ea93797dea7d53561
Sergei Bolotov <sbolotov@microsoft.com>
2020-05-25 23:11:14 -0700

    added trader args

--- app/src/main/java/com/cepgamer/strattester/Main.kt
+++ app/src/main/java/com/cepgamer/strattester/Main.kt
@@ -5,45 +5,76 @@ import java.time.Month
 import java.time.YearMonth
 
 object Main {
+    var haveCustom: Boolean = false
+    var haveMetricCutoffs: Boolean = false
+    var havePLCutoffs: Boolean = false
+    var haveInverse: Boolean = false
+
     @JvmStatic
     fun main(args: Array<String>) {
         var months = YearMonth.of(2020, Month.JANUARY) to YearMonth.of(2020, Month.MAY)
         var stock = "TVIX"
         for (arg in args) {
-            if (arg.startsWith("-m")) {
-                val pairAsList = arg
-                    .substring(2)
-                    .trim().trim { it == '=' }
-                    .split("-")
-                    .map {
-                        val mmyy = it.split('/')
-                        YearMonth.of(mmyy[1].toInt(), mmyy[0].toInt())
-                    }
-                months = pairAsList[0] to pairAsList[1]
-            } else if (arg.startsWith("-s")) {
-                stock = arg
-                    .substring(2)
-                    .trim().trim { it == '=' }
-            } else if (arg.startsWith("-h")) {
-                println(
-                    """
-                    Parameters:
-                    -m - time in the following format:
-                      mm/yy-mm/yy
-                    Where first date represents start date (inclusive), second represents end date (non-inclusive).
-                    NOTE no more than a year time frame for now.
-                    
-                    -s stock - stock on which to run tests.
+            when {
+                arg.startsWith("-m") -> {
+                    val pairAsList = arg
+                        .substring(2)
+                        .trim().trim { it == '=' }
+                        .split("-")
+                        .map {
+                            val mmyy = it.split('/')
+                            YearMonth.of(mmyy[1].toInt(), mmyy[0].toInt())
+                        }
+                    months = pairAsList[0] to pairAsList[1]
+                }
+                arg.startsWith("-s") -> {
+                    stock = arg
+                        .substring(2)
+                        .trim().trim { it == '=' }
+                }
+                arg == "-c" ->
+                    haveCustom = true
+                arg == "-mc" ->
+                    haveMetricCutoffs = true
+                arg == "-pl" ->
+                    haveCustom = true
+                arg == "-i" ->
+                    haveInverse = true
+                arg == "-h" -> {
+                    println(
+                        """
+                            Parameters:
+                            -m - time in the following format:
+                              mm/yy-mm/yy
+                            Where first date represents start date (inclusive), second represents end date (non-inclusive).
+                            NOTE no more than a year time frame for now.
+                            
+                            -s stock - stock on which to run tests.
+                            
+                            Traders:
+                            -c - include custom traders.
+                            -mc - include metric cutoff strategy traders.
+                            -pl - include profit/loss lock traders.
+                            -i - include inverse traders.
 
-                    -h - show this message.
-                """
-                )
-                return
+                            -h - show this message.
+                        """
+                    )
+                    return
+                }
             }
         }
 
 
 
-        StrategyTestingScenario(stock, months.first, months.second, true, false, false, false).runStrategyTests()
+        StrategyTestingScenario(
+            stock,
+            months.first,
+            months.second,
+            haveCustom,
+            haveMetricCutoffs,
+            havePLCutoffs,
+            haveInverse
+        ).runStrategyTests()
     }
 }
